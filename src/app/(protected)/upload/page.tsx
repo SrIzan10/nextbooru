a216a503d8c11e96c43bf19449fdd3e4e63c0532
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-01-11 16:15:07 +0100

    chore: change primary color to red

--- src/app/(protected)/upload/page.tsx
+++ src/app/(protected)/upload/page.tsx
@@ -1,46 +1,8 @@
-'use client';
-
-import { UniversalForm } from '@/components/app/UniversalForm/UniversalForm';
-import UploadFile from '@/components/app/UploadFile/UploadFile';
-import { create } from '@/lib/form/actions';
-import { useRouter } from 'next/navigation';
+import UploadPage from "@/components/app/UploadPage/UploadPage";
 
 export default function Page() {
-  const router = useRouter();
-  return (
-    <div className="min-h-[calc(100vh-4rem)] flex items-center justify-center">
-      <div className="max-w-md w-full p-4">
-        <UniversalForm
-          fields={[
-            {
-              name: 'asdf',
-              label: 'asdf',
-              type: 'hidden',
-              placeholder: 'asdf',
-              customComponent: <UploadFile id="file" />,
-            },
-            {
-              name: 'caption',
-              label: 'Caption',
-              type: 'text',
-              placeholder: 'Caption',
-              textArea: true,
-            },
-            {
-              name: 'tags',
-              label: 'Tags',
-              type: 'text',
-              placeholder: 'Tags',
-              description: 'Separate tags with commas',
-            },
-          ]}
-          schemaName="upload"
-          action={create}
-          submitText="Upload"
-          submitClassname="w-full"
-          onActionComplete={(res) => res.success && router.push(`/post/${res.id}`)}
-        />
-      </div>
-    </div>
-  );
+  if (process.env.DISABLE_UPLOADS === 'true') {
+    return <h2>Uploads are disabled in this instance.</h2>;
+  }
+  return <UploadPage />;
 }
